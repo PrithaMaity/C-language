d0485d76ffaabafe834e5ae3c52ae489b34f79f4
Pritha Maity <117790222+PrithaMaity@users.noreply.github.com>
2025-04-08 23:54:33 +0530

    Add files via upload

---
+++ 1_first.c
@@ -0,0 +1,10 @@
+#include<stdio.h> // printf and scanf is stored in this library function
+int main()
+{
+    // variable is case sensitive in c language
+    int a;
+    printf("Enter value: ");
+    scanf("%d", &a); //take user input
+    printf("Hello World!");
+    return 0;
+}
