9ccf6caac424d0bce262fca6c1fad94d0d76efb9
Pritha Maity <117790222+PrithaMaity@users.noreply.github.com>
2025-05-03 22:47:43 +0530

    Add files via upload

---
+++ 8_find-ASCII-value17.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+// You can find ASCII value of any character with the help of this code 
+    // A=65
+    // space=32
+    // a=97
+    // ;=59
+    // {}=123
+    char ch;
+    printf("Enter any character: ");
+    scanf("%c",&ch);
+
+    printf("%d",ch);
+    return 0;
+}
