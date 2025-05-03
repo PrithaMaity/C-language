a80557f4835c14e01f6aa34896718adc640cda41
Pritha Maity <117790222+PrithaMaity@users.noreply.github.com>
2025-05-03 22:31:13 +0530

    Add files via upload

---
+++ 6_Add-two-numbers15.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a=67, b=84, add;
+    printf("Enter two numbers: ");
+    scanf("%d%d",&a, &b);
+    add=a+b;
+    printf("Addition of two numbers: %d", add);
+    return 0;
+}
