02855580ae61d3ea79e2ef7c7a98b5eb8cd65e72
Pritha Maity <117790222+PrithaMaity@users.noreply.github.com>
2025-05-08 10:13:19 +0530

    Add files via upload

---
+++ 13_for-loop23.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    // int i; //variable can be declared here
+
+    for(int i=0; i<=10; i++) //initialization; condition; increment/decrement
+    {
+        printf("%d ", i); //display
+
+    }
+    return 0;
+}
