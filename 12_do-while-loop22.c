d387c06ba5232ef0bb8d2573f059666c578c5c7e
Pritha Maity <117790222+PrithaMaity@users.noreply.github.com>
2025-05-08 10:12:56 +0530

    Add files via upload

---
+++ 12_do-while-loop22.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int i=10; //initialization
+
+    do{
+        printf("%d ", i);
+        i--; //decrement
+    }
+    while(i>=1); //condition
+    return 0;
+}
